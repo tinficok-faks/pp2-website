0b1c2d34abcab3cc5b9a3aa4b16e8bcf13594d0c
tin <tin@mail.com>
2025-10-31 15:24:00 +0100

    Adding Logout button for Auth0

--- vite-project/src/features/authentication/auth-login.tsx
+++ vite-project/src/features/authentication/auth-login.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import type { LoginButtonProps } from "./auth-login-type";
+import type { AuthButtonType } from "./auth-button-type";
 
-const LoginButton : React.FC<LoginButtonProps> = ({onClick}) => {
+const LoginButton : React.FC<AuthButtonType> = ({onClick}) => {
     const {loginWithRedirect} = useAuth0();
 
     return <button onClick={(e) => {onClick?.(e); loginWithRedirect()}}>Login</button>
